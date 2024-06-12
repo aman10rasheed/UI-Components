1af0ed5762385e268f9490871fb1dec19f4af2f5
amanrasheed <amanrasheed@cygnonex.com>
2024-06-13 00:09:52 +0530

    Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import Button from "./components/Botton";
+// import Button from "./components/Botton";
+import Loader from "./components/loader/Loder";
 
 function App() {
   return (
     <>
-      <div
-        className="flex items-center gap-4 font-semibold
-    "
-      >
-        <Button variant="primary" size="sm">
-          Add items
-        </Button>
-        <Button variant="secondary" size="md">
-          Add items
-        </Button>
-        <Button variant="tertiary" size="lg">
-          Add items
-        </Button>
-        <Button variant="fourthiary" size="xl">
-          Add items
-        </Button>
-      </div>
+      <Loader />
     </>
   );
 }
---
+++ src/components/loader/Loder.tsx
@@ -0,0 +1,13 @@
+type Props = {};
+
+function Loader({}: Props) {
+  return (
+    <div>
+      <div className="spinner"></div>
+      <div className="spinner1"></div>
+      <div className="spinner2"></div>
+    </div>
+  );
+}
+
+export default Loader;
